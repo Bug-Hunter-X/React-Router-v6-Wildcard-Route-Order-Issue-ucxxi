5fbbee89bdb219fc54ed8c462752637125429a41
BugHunter-X <askzenai@gmail.com>
2025-02-21 04:12:05 +0100

    Add App.js file

---
+++ App.js
@@ -0,0 +1,21 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* This route will never match because it's a wildcard that will always be matched by the previous wildcard route*/}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() { return <h1>Home</h1>; }
+function About() { return <h1>About</h1>; }
+function NotFound() { return <h1>404</h1>; }
+export default App;
+```
